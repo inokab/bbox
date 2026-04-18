1c8d7074f6f1b7a2184a3fa369b98f13485b35df
inokab <inokabalazs@gmail.com>
2026-04-18 15:00:24 +0200

    Add OpenAPI documentation and Swagger UI for Payment API

--- api/routes/web.php
+++ api/routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/docs', function () {
+    return file_get_contents(base_path('docs/index.html'));
+});
+
+Route::get('/docs/openapi.yaml', function () {
+    return response()->file(base_path('docs/openapi.yaml'), [
+        'Content-Type' => 'application/yaml',
+    ]);
+});
